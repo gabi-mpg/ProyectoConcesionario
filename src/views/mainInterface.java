ad84af80fa6b4efa6df0566fc959f9f20f144ac0
delcorral07 <christiandelcorral00@gmail.com>
2021-11-03 14:50:15 +0000

    Escribí un método estático en Utilidades para mostrar JOption con título, mensaje e ícono más facil en el resto de clases. Checatelo ahí. También empecé a darle a la interfaz general, hice un método que añade 4 paneles internos a cada uno de los paneles de JTabbedPane para dentro de esos paneles

--- src/views/mainInterface.java
+++ src/views/mainInterface.java
@@ -65,10 +65,12 @@ public class mainInterface extends JFrame {
         panelInsertar.setBorder(BorderFactory.createLineBorder(Color.gray));
         JPanel panelEliminar = new JPanel();
         panelEliminar.setBorder(BorderFactory.createLineBorder(Color.gray));
-        panel.add(panelConsultar);
         panel.add(panelInsertar);
+        panel.add(panelConsultar);
         panel.add(panelModificar);
         panel.add(panelEliminar);
+        String[] valores = {"Matricula","Marca","Color","Direccion"};
+        panelInsertar(panelInsertar,"Motos",valores);
     }
 
     private void compPanelUsuario(){
@@ -88,6 +90,40 @@ public class mainInterface extends JFrame {
         gestor.insets = new Insets(top, left, bottom,rigth);
         gestor.anchor = pos;
         panel.add(comp,gestor);
+    }
+
+    private void panelInsertar(JPanel panelPadre, String campo, String[] valores){
+        panelPadre.setLayout(new GridBagLayout());
+        JLabel titulo = new JLabel("Insertar "+campo);
+        JLabel etiqueta1 = new JLabel(valores[0]);
+        JLabel etiqueta2 = new JLabel(valores[1]);
+        JLabel etiqueta3 = new JLabel(valores[2]);
+        JLabel etiqueta4 = new JLabel(valores[3]);
+        JTextField campo1 = new JTextField();
+        JTextField campo2 = new JTextField();
+        JTextField campo3 = new JTextField();
+        JTextField campo4 = new JTextField();
+        titulo.setPreferredSize(new Dimension(50,80));
+        etiqueta1.setPreferredSize(new Dimension(80,50));
+        etiqueta2.setPreferredSize(new Dimension(80,50));
+        etiqueta3.setPreferredSize(new Dimension(80,50));
+        etiqueta4.setPreferredSize(new Dimension(80,50));
+        campo1.setPreferredSize(new Dimension(120,20));
+        campo2.setPreferredSize(new Dimension(120,20));
+        campo3.setPreferredSize(new Dimension(120,20));
+        campo4.setPreferredSize(new Dimension(120,20));
+        anadir(titulo,panelPadre,0,0,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(campo1,panelPadre,0,1,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(campo2,panelPadre,0,2,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(campo3,panelPadre,0,3,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(campo4,panelPadre,0,4,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(etiqueta1,panelPadre,1,1,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(etiqueta2,panelPadre,1,2,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(etiqueta3,panelPadre,1,3,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+        anadir(etiqueta4,panelPadre,1,4,1,1,5,5,5,5,GridBagConstraints.LINE_START);
+
+
+
     }
 
 }
