6b58fb76b7050fb8e582e6b995102f179496ca61
delcorral07 <christiandelcorral00@gmail.com>
2021-11-12 14:04:17 +0000

    Refactorización de la clase mainInterface.java

--- src/utils/estaticas.java
+++ src/utils/estaticas.java
@@ -4,8 +4,15 @@ import java.io.File;
 
 public class estaticas {
 
+    //Variable que almacena la ruta de la carpeta de imágenes
     public static final String RUTA_IMAGENES = System.getProperty("user.dir")+
             File.separator+"src"+File.separator+"views"+File.separator
             +"imagenes"+ File.separator;
+
+    //Variables para identificar paneles en mainInterface
+    public static final String PANEL_USUARIO = "usuario";
+    public static final String PANEL_CLIENTE = "cliente";
+    public static final String PANEL_MOTO = "moto";
+    public static final String PANEL_VENTA = "venta";
 
 }
